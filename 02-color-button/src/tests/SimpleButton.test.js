f36aed02f9b44d117ab33141210cfc8b6a4d5c8f
Gabe Bravo <gabecbravo@gmail.com>
2021-03-29 17:06:18 -0400

    made helper function to get the testing elements

--- 02-color-button/src/tests/SimpleButton.test.js
+++ 02-color-button/src/tests/SimpleButton.test.js
@@ -1,72 +1,65 @@
 import { render, screen, fireEvent, prettyDOM } from '@testing-library/react';
 import SimpleButton from '../components/SimpleButton';
 
-const getButton = () => {
+const getElements = () => {
   render(<SimpleButton />);
   const colorBtn = screen.getByRole('button', { name: /change to red/i });
-  return colorBtn;
+  const checkbox = screen.getByRole('checkbox');
+  return { colorBtn, checkbox };
 };
 
 describe('red/blue button tests', () => {
   test('button has correct initial text', () => {
     // YOU CAN LOG ANY ELM LIKE THIS
     // console.log(prettyDOM(colorBtn));
-    const colorBtn = getButton();
+    const { colorBtn, checkbox } = getElements();
     expect(colorBtn).toHaveTextContent(/change to red/i); // exact button text matches
   });
 
   test('button has correct initial color', () => {
-    const colorBtn = getButton();
+    const { colorBtn, checkbox } = getElements();
     expect(colorBtn).toHaveStyle({ backgroundColor: 'blue' }); // ASSERTION : check the elemnt style
   });
 
   test('button turns blue when clicked', () => {
-    const colorBtn = getButton();
+    const { colorBtn, checkbox } = getElements();
     fireEvent.click(colorBtn); // IMPORTANT : fires the click event on button
     expect(colorBtn).toHaveStyle({ backgroundColor: 'red' }); // check the element style CHANGED
     expect(colorBtn.textContent).toBe('Change to Blue'); // check the exact text of the button
   });
 
   test('initial conditions', () => {
-    const colorBtn = getButton();
+    const { colorBtn, checkbox } = getElements();
     expect(colorBtn).toBeEnabled(); // checks button is enabled
-
-    // checkbox starts unchecked
-    const checkboxElm = screen.getByRole('checkbox');
-    expect(checkboxElm).not.toBeChecked(); // checks the checkbox starts unchecked, uses not for assertion
+    expect(checkbox).not.toBeChecked(); // checks the checkbox starts unchecked, uses not for assertion
   });
 });
 
 describe('check box tests', () => {
   test('checkbox toggles button from enabled to disabled', () => {
-    const colorBtn = getButton();
+    const { colorBtn, checkbox } = getElements();
 
-    // checkbox disables button
-    const checkboxElm = screen.getByRole('checkbox');
-    fireEvent.click(checkboxElm); // IMPORTANT : fires the click event on checkbox
+    fireEvent.click(checkbox); // IMPORTANT : fires the click event on checkbox
     expect(colorBtn).toBeDisabled(); // checks button is disabled
 
     // checkbox disables button
-    fireEvent.click(checkboxElm);
+    fireEvent.click(checkbox);
     expect(colorBtn).toBeEnabled(); // checks button is enabled
   });
 
   test('change button color, then disable, then change back', () => {
-    const colorBtn = getButton();
-    const checkboxElm = screen.getByRole('checkbox', {
-      name: /disable button/i,
-    });
+    const { colorBtn, checkbox } = getElements();
 
     // check button toggle changes color
     fireEvent.click(colorBtn);
     expect(colorBtn).toHaveStyle({ backgroundColor: 'red' });
 
     // check disable checkbox turns button gray
-    fireEvent.click(checkboxElm);
+    fireEvent.click(checkbox);
     expect(colorBtn).toHaveStyle({ backgroundColor: 'gray' });
 
     // check disable checkbox turns button back to red
-    fireEvent.click(checkboxElm);
+    fireEvent.click(checkbox);
     expect(colorBtn).toHaveStyle({ backgroundColor: 'red' });
   });
 });
