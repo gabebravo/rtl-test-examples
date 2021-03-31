0f156822bd6f3e769da0259ba8b20e565eeb45bd
Gabe Bravo <gabecbravo@gmail.com>
2021-03-30 21:37:54 -0400

    Added the popover test

--- 04-form-popover/src/views/summary/test/SummaryForm.test.jsx
+++ 04-form-popover/src/views/summary/test/SummaryForm.test.jsx
@@ -30,3 +30,26 @@ test('Checkbox enables button on first click and disables on second click', () =
   userEvent.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
+
+test('popover responds to hover', async () => {
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
+});
